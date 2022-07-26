f7017a7a231e3d34d15045685e55bfe261630a14
claunicole <claunicole.gf@gmail.com>
2022-07-25 21:53:08 -0400

    Page 404

--- src/components/FlightList/FlightList.jsx
+++ src/components/FlightList/FlightList.jsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom"
 import Flight from "../Flight/Flight"
+import "./FlightList.css"
 
 
 function FlightList({data}) {
   return (
     <div>
-      {data.map(flight => <Flight id={flight.id} flight={flight}/>)} 
+      {data.length 
+          ? data.map((flight) => <Flight id={flight.id} flight={flight}/>)             
+          : <div className="page404">
+            <p className="title">404 page not found</p>
+            <Link to="/">
+            <p>Seguir buscando vuelos</p>
+            </Link>
+          </div>
+          }    
     </div>
   )
 }
